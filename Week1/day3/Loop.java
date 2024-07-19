91e879400b93bcc4ec33eace194cac99dbcae296
Sachin <86413545+SriSachin21@users.noreply.github.com>
2024-07-19 11:16:19 +0530

    Add files via upload

---
+++ Week1/day3/Loop.java
@@ -0,0 +1,12 @@
+package aWeek1.day3;
+
+public class Loop {
+	public static void main(String[] args) {
+		for(int i=11;i<=20;i++ ) {
+			if(i%2==0) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
